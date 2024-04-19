605d624c9418874ca8238886f7dd4d8ec3b6bc98
masha <exxxxxxe90@gmail.com>
2024-04-19 16:01:36 +0300

    new object

---
+++ ElbrusTech/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ElbrusTech
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
